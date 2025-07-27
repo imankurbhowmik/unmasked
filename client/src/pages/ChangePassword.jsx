1c65f87d97924763460fc2392355efda91a3c4fe
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2025-07-27 08:56:18 +0530

    fix header

--- client/src/pages/ChangePassword.jsx
+++ client/src/pages/ChangePassword.jsx
@@ -55,8 +55,9 @@ const ChangePassword = () => {
   };
 
   return (
+    <>
+    <Header />
     <div className="min-h-screen w-full bg-gray-900 text-white px-4">
-      <Header />
       <main className="max-w-md mx-auto p-6 mt-8 bg-gray-800 rounded-2xl shadow-md space-y-6">
         <h2 className="text-xl font-bold mb-4">Change Password</h2>
 
@@ -104,6 +105,7 @@ const ChangePassword = () => {
         </form>
       </main>
     </div>
+    </>
   );
 };
 
